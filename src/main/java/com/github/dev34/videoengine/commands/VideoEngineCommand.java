0e717a720b2e385b790fcffab34354ac1fe7f8ee
Dev Patel <devvp08@gmail.com>
2026-02-16 10:46:38 -0600

    Rename videoengine-example to videoengine-example.gif

--- src/main/java/com/github/dev34/videoengine/commands/VideoEngineCommand.java
+++ src/main/java/com/github/dev34/videoengine/commands/VideoEngineCommand.java
@@ -14,20 +14,24 @@ public class VideoEngineCommand implements CommandExecutor {
             "&7---------------&8[&a VideoEngine &8]&7---------------\n" +
                     "&a/videoengine play <video> <duration> [player] &8-&f Start playing a video (duration in seconds)\n" +
                     "&a/videoengine stop [player] &8- &fStop playing a video\n" +
+                    "&a/videoengine setup &8- &fSee setup instructions\n" +
                     "&a/videoengine help &8-&f See this list of commands\n");
 
     private static final String playPerm = "videoengine.play";
     private static final String stopPerm = "videoengine.stop";
     private static final String playOthersPerm = "videoengine.play.others";
     private static final String stopOthersPerm = "videoengine.stop.others";
+    private static final String adminPerm = "videoengine.admin";
 
-    private boolean checkPermission(Player player, String permission) {
-        if (!player.hasPermission(permission)) {
-            player.sendMessage(VideoEnginePlugin.colorize("&cYou do not have permission to use this command! (" + permission + ")"));
-            return false;
-        }
-        return true;
-    }
+    private static final String playCommandUsage = VideoEnginePlugin.colorize("&cUsage: /videoengine play <video> <duration in seconds> [player]");
+
+    private static final String noPermsMessage = VideoEnginePlugin.colorize("&cYou do not have permission to use this command!");
+
+    private static final String setupMessage = VideoEnginePlugin.colorize(
+            "&aSetup Instructions:\n" +
+            "&f1. Upload a video on the converter tool: &ahttps://videoengine.netlify.app\n" +
+            "&f2. Download the resource pack and place it in your resource pack folder (Options > Resource Packs > Open Pack Folder)\n" +
+            "&f3. Run the command: &a/videoengine play <video name> <time in seconds>");
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
@@ -47,14 +51,27 @@ public class VideoEngineCommand implements CommandExecutor {
         String subcommand = args[0].toLowerCase();
 
         switch (subcommand) {
+
+            case "setup":
+                if (!player.hasPermission(adminPerm)){
+                    player.sendMessage(noPermsMessage + " (" + adminPerm + ")");
+                    return true;
+                }
+                player.sendMessage(setupMessage);
+                break;
             case "play": {
                 if (args.length < 3) {
-                    player.sendMessage(VideoEnginePlugin.colorize("&cUsage: /videoengine play <video> <duration in seconds> [player]"));
+                    player.sendMessage(playCommandUsage);
                     return true;
                 }
 
                 String video = args[1];
                 int durationSeconds;
+
+                if (args[2] == null){
+                    player.sendMessage(playCommandUsage);
+                    return true;
+                }
 
                 try {
                     durationSeconds = Integer.parseInt(args[2]);
@@ -71,9 +88,14 @@ public class VideoEngineCommand implements CommandExecutor {
                 }
 
                 if (!player.equals(target)) {
-                    if (!checkPermission(player, playOthersPerm)) return true;
+                    if (!player.hasPermission(playOthersPerm) && !player.hasPermission(adminPerm)) {
+                        player.sendMessage(noPermsMessage + " (" + playOthersPerm + ")");
+                    }
                 } else {
-                    if (!checkPermission(player, playPerm) && !checkPermission(player, playOthersPerm)) return true;
+                    if (!player.hasPermission(playPerm) && !player.hasPermission(playOthersPerm) && !player.hasPermission(adminPerm)) {
+                        player.sendMessage(noPermsMessage + " (" + playPerm + ")");
+                        return true;
+                    }
                 }
 
                 if (VideoPlayer.getViewers().containsKey(target)) {
@@ -84,8 +106,8 @@ public class VideoEngineCommand implements CommandExecutor {
                 player.sendMessage(VideoEnginePlugin.colorize("&aPlaying video &f'" + video + "'&a for &f" + target.getName() + "&a for &f" + durationSeconds + " seconds"));
 
                 VideoPlayer.displayFrames(target, video, durationSeconds * 20);
-                break;
-            }
+                break; }
+
             case "stop": {
                 Player target = (args.length >= 2) ? Bukkit.getPlayerExact(args[1]) : player;
 
@@ -95,9 +117,15 @@ public class VideoEngineCommand implements CommandExecutor {
                 }
 
                 if (!player.equals(target)) {
-                    if (!checkPermission(player, stopOthersPerm)) return true;
+                    if (!player.hasPermission(stopOthersPerm) && !player.hasPermission(adminPerm)) {
+                        player.sendMessage(noPermsMessage + " (" + stopOthersPerm+ ")");
+                        return true;
+                    }
                 } else {
-                    if (!checkPermission(player, stopPerm) && !checkPermission(player, stopOthersPerm)) return true;
+                    if (!player.hasPermission(stopPerm) && !player.hasPermission(stopOthersPerm) && !player.hasPermission(adminPerm)) {
+                        player.sendMessage(noPermsMessage + " (" + stopPerm + ")");
+                        return true;
+                    }
                 }
 
                 if (!VideoPlayer.getViewers().containsKey(target)) {
@@ -107,8 +135,8 @@ public class VideoEngineCommand implements CommandExecutor {
 
                 player.sendMessage(VideoEnginePlugin.colorize("&aVideo has been stopped for " + target.getName()));
                 VideoPlayer.stopVideo(target);
-                break;
-            }
+                break; }
+
             default:
                 player.sendMessage(helpMessage);
         }
